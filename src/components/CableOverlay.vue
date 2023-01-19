56e751a0b91a97765cb3a3bde5ff7cf7b9d3568c
evvvritt <evv.info@gmail.com>
2023-01-19 18:46:04 +0100

    use CableOverlay.vue

---
+++ src/components/CableOverlay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div ref="overlayEl" v-if="$props.id" class="fixed overlay z-40 bg-white overflow-scroll scrollbars-hidden" tabindex="0">
+    <CableImage :id="$props.id"></CableImage>
+
+    <button class="fixed top-0 right-0 p-8 mouse_hover_bg-yellow-500" to="/" @click="closeOverlay">
+      <span class="sr-only">Close</span>
+      <SVGX class="w-6 h-6 text-black" strokeWidth="1.15" />
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import SVGX from '../components/SVG-X.vue';
+  import CableImage from '../components/CableImage.vue';
+  import store from '../store';
+  
+  const props = defineProps(['id'])
+
+  let lastActiveEl
+  
+  const overlayEl = ref()
+
+  function closeOverlay () {
+    lastActiveEl.focus()
+    store.commit('CLOSE_FULLSCREEN')
+  }
+
+  onMounted(() => {
+    lastActiveEl = document.activeElement
+    overlayEl.value.focus()
+    overlayEl.value.scrollTo(0, (overlayEl.value.scrollHeight - window.innerHeight)/2);
+  })
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,37 +8,15 @@
 </router-view>
 
 <!-- fullscreen overlay -->
-<div ref="overlayEl" v-if="store.state.fullscreenId" class="fixed overlay z-40 bg-white overflow-scroll scrollbars-hidden" tabindex="0">
-  <CableImage :id="store.state.fullscreenId"></CableImage>
-
-  <button class="fixed top-0 right-0 p-8 mouse_hover_bg-yellow-500" to="/" @click="closeOverlay">
-    <span class="sr-only">Close</span>
-    <SVGX class="w-6 h-6 text-black" strokeWidth="1.15" />
-    <img class="hidden" src="nope" @error="onOverlayEnter" />
-  </button>
-</div>
+<CableOverlay v-if="$store.state.fullscreenId" :id="$store.state.fullscreenId"></CableOverlay>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import CableOverlay from '../components/CableOverlay.vue';
 import Map from '../components/Map.vue'
-import SVGX from '../components/SVG-X.vue';
-import CableImage from '../components/CableImage.vue';
-import store from '../store';
 
 const mintsVisible = ref(false)
-
-let lastActiveEl
-const overlayEl = ref()
-function onOverlayEnter () {
-  lastActiveEl = document.activeElement
-  overlayEl.value.focus()
-  overlayEl.value.scrollTo(0, (overlayEl.value.scrollHeight - window.innerHeight)/2);
-} 
-function closeOverlay () {
-  lastActiveEl.focus()
-  store.commit('CLOSE_FULLSCREEN')
-}
 </script>
 
 <style>

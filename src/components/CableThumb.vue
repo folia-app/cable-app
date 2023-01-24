9056d1524064d747a95723e95e38c3ad7473374f
evvvritt <evv.info@gmail.com>
2023-01-24 19:59:54 +0100

    highlight YOU'r thumbs yellow

--- src/components/CableThumb.vue
+++ src/components/CableThumb.vue
@@ -37,7 +37,11 @@
           <h6 class="font-bold text-sm inline-block" style="margin-right:0.75em">{{ cableData.name }}</h6>
           <div class="inline-block uppercase">{{ cableData.length.split(' ')[0] }}&nbsp;<span class="text-smaller">KM</span></div>
         </div>
-        <li><span class="text-smaller">owned by</span> <template v-if="owner"><a class="text-2xs leading-none underline font-bold" :href="$store.getters.marketplaceLink({ account: owner })" target="_blank" rel="noopener noreferrer"><Addr :address="owner"></Addr></a></template><template v-else><Observer class="inline" @visible="getOwner"><span class="animate-pulse">...</span></Observer></template></li>
+        <li>
+          <div class="px-2 -ml-2 inline-block" :class="{'bg-yellow-500 rounded': owner && owner.toLowerCase() === $store.state.address}">
+            <span class="text-smaller">owned by</span> <template v-if="owner"><a class="text-2xs leading-none underline font-bold" :href="$store.getters.marketplaceLink({ account: owner })" target="_blank" rel="noopener noreferrer"><Addr :address="owner" :youOn="true"></Addr></a></template><template v-else><Observer class="inline" @visible="getOwner"><span class="animate-pulse">...</span></Observer></template>
+          </div>
+        </li>
         <!-- <li class="align-baseline">
           {{ cableData.length.split(' ')[0] }} <span class="text-smaller">KM</span>
         </li> -->
